853d97589a55be2c5b05ca34fdbb93263dbafa4d
Ryan Kwok <rkwok2@nait.ca>
2017-11-16 10:43:07 -0700

    working on q1

--- Rkwok2Ica14.sql
+++ Rkwok2Ica14.sql
@@ -1,3 +1,27 @@
 --CMPE2400 Ica 14 Rkwok2 --
 
 --q1--
+--make sure procedure is found and exists
+if exists
+(
+select *
+from sysobjects
+where name = 'ica14_01' --give it a name that it should be looking for to see if it exists
+)
+--Call Procedure with this name
+drop procedure ica14_01
+go
+
+
+--What is the procedure, create it here
+create procedure ica14_01
+
+as
+select TOP 5 ProductName as 'Product Name', Quantity 
+from NorthwindTraders.dbo.Products left outer join NorthwindTraders.dbo.[Order Details]
+	 on Products.ProductID = [Order Details].ProductID
+order by Quantity asc
+go
+
+--Execute procedure
+exec 
