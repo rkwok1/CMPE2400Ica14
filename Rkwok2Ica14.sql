82b0e33156ba0a87cf6390e9af9f2124322832b1
Ryan Kwok <rkwok2@nait.ca>
2017-11-16 11:55:14 -0700

    still going

--- Rkwok2Ica14.sql
+++ Rkwok2Ica14.sql
@@ -15,13 +15,19 @@ go
 
 --What is the procedure, create it here
 create procedure ica14_01
-
+--Put parameters here (note: if you don't know what type the data needs to be, Check your tables in the database!!!)
+@category as nvarchar(30) = 'Beverages'
 as
-select TOP 5 ProductName as 'Product Name', Quantity 
+select TOP 1 @category as 'Category', ProductName as 'Product Name', Quantity as 'Highest Quantity'
 from NorthwindTraders.dbo.Products left outer join NorthwindTraders.dbo.[Order Details]
 	 on Products.ProductID = [Order Details].ProductID
-order by Quantity asc
+	 group by ProductName,Quantity
+	 order by Quantity Desc
 go
 
 --Execute procedure
-exec 
+exec ica14_01
+go
+
+exec ica14_01 'Confections'
+go
