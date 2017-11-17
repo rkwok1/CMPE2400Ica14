9c1263828abd327b96fa2d22b0010def4d83b995
Ryan Kwok <rkwok2@nait.ca>
2017-11-17 11:35:28 -0700

    q2 is done

--- Rkwok2Ica14.sql
+++ Rkwok2Ica14.sql
@@ -19,8 +19,11 @@ create procedure ica14_01
 @category as nvarchar(30) = 'Beverages'
 as
 select TOP 1 @category as 'Category', ProductName as 'Product Name', Quantity as 'Highest Quantity'
-from NorthwindTraders.dbo.Products left outer join NorthwindTraders.dbo.[Order Details]
-	 on Products.ProductID = [Order Details].ProductID
+from NorthwindTraders.dbo.Products as P left outer join NorthwindTraders.dbo.[Order Details] as OD
+	 on P.ProductID = OD.ProductID
+	 inner join NorthwindTraders.dbo.Categories as C
+	 on P.CategoryID = C.CategoryID
+	 where @category = C.CategoryName
 	 group by ProductName,Quantity
 	 order by Quantity Desc
 go
@@ -31,3 +34,48 @@ go
 
 exec ica14_01 'Confections'
 go
+
+
+
+--q2--
+--make sure procedure is found and exists
+if exists
+(
+select *
+from sysobjects
+where name = 'ica14_02' --give it a name that it should be looking for to see if it exists
+)
+--Call Procedure with this name
+drop procedure ica14_02
+go
+
+--Create procedure
+create procedure ica14_02
+--arguments for procedure
+@year as int,
+@name as nvarchar(64),
+@freight as money
+as
+select Year(OrderDate) as 'Year',
+ (LastName + ', ' + FirstName) as 'Name',
+ AVG(Freight) as 'Biggest Avg Freight'
+
+ from NorthwindTraders.dbo.Employees as E
+	left outer Join NorthwindTraders.dbo.Orders as O
+	on E.EmployeeID = O.EmployeeID
+
+where ( YEAR(OrderDate) = @year) AND ((LastName +', '+ FirstName) = @name)  
+
+group by LastName, FirstName, Year(OrderDate)--Group non aggregate values
+
+having AVG(Freight) < @freight
+
+order by AVG(Freight) desc
+
+go
+
+exec ica14_02 1996, 'Buchanan, Steven', 200
+exec ica14_02 1997, 'Leverling, Janet', 100
+go
+
+--q3--
